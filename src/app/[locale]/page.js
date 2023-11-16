002314a4c97701ef7620c5e84e57247152950c6e
Dariusz Stawik <dariusz.stawik@gmail.com>
2023-11-16 16:24:01 +0100

    Setup next-intl

---
+++ src/app/[locale]/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { useTranslations } from "next-intl";
+
+export default function Home() {
+  const t = useTranslations("Index");
+  return (
+    <>
+      <div>
+        <h1>{t("title")}</h1>
+      </div>
+    </>
+  );
+}
